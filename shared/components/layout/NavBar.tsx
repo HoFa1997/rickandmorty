e9b49cdbf9b822faa61221d06b8336d45fefd018
HoFa <hfatemi1997@gmail.com>
2022-09-10 00:33:44 +0430

    refactor: :art:

--- shared/components/layout/NavBar.tsx
+++ shared/components/layout/NavBar.tsx
@@ -1,24 +1,57 @@
 import Image from "next/image";
-import { FunctionComponent, JSXElementConstructor } from "react";
-import style from "../../../styles/Navbar.module.scss";
+import { FunctionComponent } from "react";
 import logo from "../../../public/rickandmortylogo.png";
+import {
+  AppBar,
+  Box,
+  Button,
+  ButtonProps,
+  styled,
+  Toolbar,
+} from "@mui/material";
 
 interface NavBarProps {}
+
+const BtnAboutUs = styled(Button)<ButtonProps>({
+  border: `2px solid rgb(255, 152, 0)`,
+  color: `black`,
+  ":hover": {
+    color: "white",
+    border: "2px solid rgb(255, 152, 0)",
+    background: "rgb(255, 152, 0)",
+  },
+});
+
+const BtnOther = styled(Button)<ButtonProps>({
+  color: `black`,
+  background: "transparent",
+  ":hover": {
+    color: "rgb(255, 152, 0)",
+    background: "transparent",
+  },
+});
 
 const NavBar: FunctionComponent<NavBarProps> = () => {
   return (
     <>
-      <div className={style.Container}>
-        <div className={style.buttonContainer}>
-          <button className={style.textButton}>Character</button>
-          <button className={style.textButton}>Location</button>
-          <button className={style.textButton}>Episode</button>
-          <button className={style.borderButton}>About Me</button>
-        </div>
-        <div className={style.logoContainer}>
-          <Image src={logo.src} height={50} width={50} alt="rickandmortylogo" />
-        </div>
-      </div>
+      <AppBar elevation={0} color="transparent" position="static">
+        <Toolbar disableGutters sx={{ paddingX: 2 }}>
+          <Box>
+            <Image
+              src={logo.src}
+              height={50}
+              width={50}
+              alt="rickandmortylogo"
+            />
+          </Box>
+          <Box flexGrow={1} textAlign={"right"}>
+            <BtnOther>Character</BtnOther>
+            <BtnOther>Location</BtnOther>
+            <BtnOther>Episode</BtnOther>
+            <BtnAboutUs variant="outlined">About US</BtnAboutUs>
+          </Box>
+        </Toolbar>
+      </AppBar>
     </>
   );
 };
