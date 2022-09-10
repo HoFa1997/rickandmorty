a6f308af8c8ab96cf64fb252336098bc158b2417
HoFa <hfatemi1997@gmail.com>
2022-09-10 21:57:07 +0430

    refactor:

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Layout from "@/components/layout/Layout";
 import { Box, Container, Grid, Paper, useTheme } from "@mui/material";
 import CharacterCard from "@/components/homePage/charItem";
 import HeaderHomePage from "@/components/homePage/header";
+import Head from "next/head";
 
 const Home: NextPage = () => {
   const theme = useTheme();
@@ -13,30 +14,41 @@ const Home: NextPage = () => {
 
   return (
     <Layout>
-        <>
-          <HeaderHomePage />
-          <Box bgcolor="#202329" padding='5rem 0'>
-            <Container>
-                <Grid container columnSpacing={5} rowSpacing={3} textAlign="left">
-                  {data.map((char) => {
-                    return (
-                      <Grid key={char.id} item lg={6} md={6} sm={11} xs={11} margin="auto">
-                        <CharacterCard
-                          charName={char.name}
-                          charStatus={char.status}
-                          charSpecies={char.species}
-                          charLocationName={char.location.name}
-                          charType={char.species}
-                          image={char.image}
-                        />
-                      </Grid>
-                    );
-                  })}
-                </Grid>
-            </Container>
-          </Box>
-        </>
-      </Layout>
+      <>
+        <Head>
+          <title>Rick and Morty</title>
+        </Head>
+        <HeaderHomePage />
+        <Box bgcolor="#202329" padding="5rem 0">
+          <Container>
+            <Grid container columnSpacing={5} rowSpacing={3} textAlign="left">
+              {data.map((char) => {
+                return (
+                  <Grid
+                    key={char.id}
+                    item
+                    lg={6}
+                    md={6}
+                    sm={11}
+                    xs={11}
+                    margin="auto"
+                  >
+                    <CharacterCard
+                      charName={char.name}
+                      charStatus={char.status}
+                      charSpecies={char.species}
+                      charLocationName={char.location.name}
+                      charType={char.species}
+                      image={char.image}
+                    />
+                  </Grid>
+                );
+              })}
+            </Grid>
+          </Container>
+        </Box>
+      </>
+    </Layout>
   );
 };
 
