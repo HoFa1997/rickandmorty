a91917fec41916c0e47618590cd8c9723b3dc55b
HoFa <hfatemi1997@gmail.com>
2022-09-10 21:56:43 +0430

    refactor: :zap: uninstall some packages

--- shared/components/homePage/charItem.tsx
+++ shared/components/homePage/charItem.tsx
@@ -1,9 +1,8 @@
 import { Card, Grid, Typography } from "@mui/material";
 import Box from "@mui/material/Box";
-import { positions } from "@mui/system";
 import Image from "next/image";
 import { FunctionComponent } from "react";
-import CircleIcon from '@mui/icons-material/Circle';
+import CircleIcon from "@mui/icons-material/Circle";
 
 interface CharacterCardProps {
   charName: string;
@@ -23,30 +22,70 @@ const CharacterCard: FunctionComponent<CharacterCardProps> = ({
   image,
 }) => {
   return (
-    <Card sx={{ backgroundColor: '#3C3E44', borderRadius: '10px' }}>
-      <Grid  container columns={4}>
-        <Grid sx={{ position: "relative", height: { xs: 350, sm: 230 }}} item md={1.5} xs={4}>
+    <Card sx={{ backgroundColor: "#3C3E44", borderRadius: "10px" }}>
+      <Grid container columns={4}>
+        <Grid
+          sx={{ position: "relative", height: { xs: 350, sm: 230 } }}
+          item
+          md={1.5}
+          xs={4}
+        >
           <Image layout="fill" objectFit="cover" src={image} alt={charName} />
         </Grid>
         <Grid item xs={4} md={2.5}>
           <Box padding=".6rem">
             <Grid item>
-              <Typography sx={{fontWeight: '800', fontSize: '1.5rem', cursor: 'pointer',":hover": { color: '#FF9800' }}}>{charName}</Typography>
-                <Typography sx={{ fontWeight: '700', fontSize: '.9rem'}}>
-              { charStatus === 'Alive' ? (
-                  <CircleIcon sx={{fontSize: '.8rem', color: 'green', margin: '0 .3rem'}} />
-                  ) : (
-                  <CircleIcon sx={{fontSize: '.8rem', color: 'red', margin: '0 .3rem'}} />
-                  ) }
-                {`${charStatus} - ${charSpecies}`}</Typography>
+              <Typography
+                sx={{
+                  fontWeight: "800",
+                  fontSize: "1.5rem",
+                  cursor: "pointer",
+                  ":hover": { color: "#FF9800" },
+                }}
+              >
+                {charName}
+              </Typography>
+              <Typography sx={{ fontWeight: "700", fontSize: ".9rem" }}>
+                {charStatus === "Alive" ? (
+                  <CircleIcon
+                    sx={{
+                      fontSize: ".8rem",
+                      color: "green",
+                      margin: "0 .3rem",
+                    }}
+                  />
+                ) : (
+                  <CircleIcon
+                    sx={{ fontSize: ".8rem", color: "red", margin: "0 .3rem" }}
+                  />
+                )}
+                {`${charStatus} - ${charSpecies}`}
+              </Typography>
             </Grid>
             <Grid item xs padding=".9rem 0">
-              <Typography sx={{ fontWeight: '700', fontSize: '.8rem', color: '#9E9E9E' }}>Last known location:</Typography>
-              <Typography sx={{cursor: 'pointer',":hover": { color: '#FF9800' }}}>{charLocationName}</Typography>
+              <Typography
+                sx={{ fontWeight: "700", fontSize: ".8rem", color: "#9E9E9E" }}
+              >
+                Last known location:
+              </Typography>
+              <Typography
+                sx={{ cursor: "pointer", ":hover": { color: "#FF9800" } }}
+              >
+                {charLocationName}
+              </Typography>
             </Grid>
             <Grid item xs>
-              <Typography color="textSecondary" sx={{ fontWeight: '700', fontSize: '.8rem', color: '#9E9E9E' }}>Character type:</Typography>
-              <Typography sx={{cursor: 'pointer',":hover": { color: '#FF9800' }}}>{charType}</Typography>
+              <Typography
+                color="textSecondary"
+                sx={{ fontWeight: "700", fontSize: ".8rem", color: "#9E9E9E" }}
+              >
+                Character type:
+              </Typography>
+              <Typography
+                sx={{ cursor: "pointer", ":hover": { color: "#FF9800" } }}
+              >
+                {charType}
+              </Typography>
             </Grid>
           </Box>
         </Grid>
--- shared/components/layout/Footer.tsx
+++ shared/components/layout/Footer.tsx
@@ -1,35 +1,39 @@
-import styled from '@emotion/styled';
-import { Box, Grid, Typography } from '@mui/material'
-import { BoxProps, Container, ContainerProps } from '@mui/system';
-import Link from 'next/link'
+import styled from "@emotion/styled";
+import { Box, Grid, Typography } from "@mui/material";
+import { BoxProps } from "@mui/system";
+import Link from "next/link";
 
 const FooterContainer = styled(Box)<BoxProps>({
-  background: 'rgb(245, 245, 245)',
-  padding: '2rem 0',
-  color: '#202329',
-  textAlign: 'center'
+  background: "rgb(245, 245, 245)",
+  padding: "2rem 0",
+  color: "#202329",
+  textAlign: "center",
 });
-
 
 const Footer = () => {
   return (
     <footer>
-          <FooterContainer>
-          <Grid container columns={12} margin="auto">
-            <Grid md={6} xs={12} item>
-                <Typography color="#ff9800" sx={{ fontWeight: 900, cursor: 'default' }}>© Copyright - Rick & Morty</Typography>
-            </Grid>
-            <Grid md={6} xs={12} item>
-                <Link href='/'>Character</Link>
-                <small> | </small>
-                <Link href='/'>Location</Link>
-                <small> | </small>
-                <Link href='/'>Episode</Link>
-            </Grid>
+      <FooterContainer>
+        <Grid container columns={12} margin="auto">
+          <Grid md={6} xs={12} item>
+            <Typography
+              color="#ff9800"
+              sx={{ fontWeight: 900, cursor: "default" }}
+            >
+              © Copyright - Rick & Morty
+            </Typography>
           </Grid>
-        </FooterContainer>
+          <Grid md={6} xs={12} item>
+            <Link href="/">Character</Link>
+            <small> | </small>
+            <Link href="/">Location</Link>
+            <small> | </small>
+            <Link href="/">Episode</Link>
+          </Grid>
+        </Grid>
+      </FooterContainer>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
